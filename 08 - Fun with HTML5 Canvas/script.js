20b7bca4e96d865a46532f1bb8d87734f109e892
Chiang Lynn <lynnchiangtw@gmail.com>
2021-05-17 16:59:07 +0800

    Build draw function

---
+++ 08 - Fun with HTML5 Canvas/script.js
@@ -0,0 +1,34 @@
+const canvas = document.getElementById('draw');
+const ctx = canvas.getContext('2d');
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+function draw(e) {
+    if (!isDrawing) return;
+    ctx.strokeStyle = `hsl(${hue},100%,50%)`;
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    hue >= 360 ? hue = 0 : hue++;
+    ctx.lineWidth >= 100 || ctx.lineWidth <= 1 ? direction = !direction : '';
+    direction ? ctx.lineWidth++ : ctx.lineWidth--;
+}
+
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
+canvas.addEventListener('mousemove', draw);
