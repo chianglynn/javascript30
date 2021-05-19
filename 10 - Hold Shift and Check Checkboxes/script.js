b9e0e331c59f32f244144c3a7a516757941237b4
Chiang Lynn <lynnchiangtw@gmail.com>
2021-05-19 15:45:44 +0800

    Fixed solution 1 and add solution 2

--- 10 - Hold Shift and Check Checkboxes/script.js
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -1,16 +1,40 @@
-const inputs = document.querySelectorAll('input');
-const indexArr = [];
+// // Solution 1: No need to hold down shift key
+// const inputs = document.querySelectorAll('input');
+// const indexArr = [];
 
-inputs.forEach((input, index) => {
-    input.addEventListener('click', () => {
-        if (input.checked) indexArr.push(index);
-        if (!input.checked) {
-            const uncheckedIndex = indexArr.indexOf(index);
-            indexArr.splice(uncheckedIndex, 1);
-        }
-        for (let i = Math.min(...indexArr); i < Math.max(...indexArr); i++) {
-            inputs[i].checked = true;
-            if (!indexArr.includes(i)) indexArr.push(i);
-        }
+// inputs.forEach((input, index) => {
+//   input.addEventListener('click', (e) => {
+//     if (input.checked) indexArr.push(index);
+//     if (!input.checked) {
+//       const uncheckedIndex = indexArr.indexOf(index);
+//       indexArr.splice(uncheckedIndex, 1);
+//     }
+//     if (e.shiftKey) {
+//       for (let i = Math.min(...indexArr); i < Math.max(...indexArr); i++) {
+//         inputs[i].checked = true;
+//         if (!indexArr.includes(i)) indexArr.push(i);
+//       }
+//     }
+//   });
+// });
+
+// Solution 2
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+let lastChecked;
+
+function handleCheck(e) {
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach(checkbox => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
     });
-});
+  }
+  lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
