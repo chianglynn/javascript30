df7fbd127a822a80a2e62bd93fb92906764de167
Chiang Lynn <lynnchiangtw@gmail.com>
2021-05-22 20:00:35 +0800

    Build function for sliding in images

--- 13 - Slide in on Scroll/script.js
+++ 13 - Slide in on Scroll/script.js
@@ -1,3 +1,16 @@
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide() {
+    sliderImages.forEach(sliderImage => {
+        const slideInAt = window.scrollY + window.innerHeight - sliderImage.height / 2;
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if (isHalfShown && isNotScrolledPast) sliderImage.classList.add('active');
+        else sliderImage.classList.remove('active')
+    });
+}
+
 function debounce(func, wait = 20, immediate = true) {
     var timeout;
     return function () {
@@ -12,3 +25,5 @@ function debounce(func, wait = 20, immediate = true) {
         if (callNow) func.apply(context, args);
     };
 }
+
+window.addEventListener('scroll', debounce(checkSlide));
