ff4661d30c3eb3946da8fb3f04af03210c0d8f00
Chiang Lynn <lynnchiangtw@gmail.com>
2021-05-29 13:17:06 +0800

    Build functions for speech recognition

--- 20 - Speech Detection/script.js
+++ 20 - Speech Detection/script.js
@@ -1,1 +1,25 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const words = document.querySelector('.words');
+let p = document.createElement('p');
+const recognition = new SpeechRecognition();
+// recognition.lang = 'en-US';
+
+recognition.interimResults = true;
+words.appendChild(p);
+
+recognition.addEventListener('result', (e) => {
+    console.log(e.results[0]);
+    const transcript = [...e.results]
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('');
+    p.textContent = transcript;
+    if (e.results[0].isFinal) {
+        p = document.createElement('p');
+        words.appendChild(p);
+    }
+});
+
+recognition.start();
+recognition.addEventListener('end', recognition.start);
