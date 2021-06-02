2db4065378e40bfe2b330c110f6c4314babc9173
Chiang Lynn <lynnchiangtw@gmail.com>
2021-06-02 16:10:46 +0800

    Add solution2: using intersection observer instead of event listener

--- 24 - Sticky Nav/script.js
+++ 24 - Sticky Nav/script.js
@@ -1,14 +1,29 @@
 const nav = document.getElementById('main');
-const topOfNav = nav.offsetTop;
 
-function fixNav() {
-    if (window.scrollY >= topOfNav) {
-        document.body.style.paddingTop = nav.offsetHeight + 'px';
-        document.body.classList.add('fixed-nav');
-    } else {
-        document.body.style.paddingTop = 0;
-        document.body.classList.remove('fixed-nav');
-    }
-}
+// Solution 1
+// const topOfNav = nav.offsetTop;
+// function fixNav() {
+//     if (window.scrollY >= topOfNav) {
+//         document.body.style.paddingTop = nav.offsetHeight + 'px';
+//         document.body.classList.add('fixed-nav');
+//     } else {
+//         document.body.style.paddingTop = 0;
+//         document.body.classList.remove('fixed-nav');
+//     }
+// }
+// window.addEventListener('scroll', fixNav);
 
-window.addEventListener('scroll', fixNav);
+// Solution 2
+const header = document.querySelector('header');
+const navHeight = nav.getBoundingClientRect().height;
+const fixNav = entries => {
+    const [entry] = entries;
+    if (!entry.isIntersecting) document.body.classList.add('fixed-nav');
+    else document.body.classList.remove('fixed-nav');
+};
+const headerObserver = new IntersectionObserver(fixNav, {
+    root: null,
+    threshold: 0,
+    // rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
